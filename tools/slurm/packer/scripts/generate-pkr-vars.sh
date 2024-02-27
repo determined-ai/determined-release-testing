c2f374b13c4913c8096cf590d7692529de05118a
Jagadeesh Madagundi <jagadeesh545@gmail.com>
2024-02-27 17:39:01 -0600

    fix: update generate-pkr-vars.sh script to use the new location for launcher debian file (#1199)
    * fix: update generate-pkr-vars.sh script to use the new location for launcher debian file

--- tools/slurm/packer/scripts/generate-pkr-vars.sh
+++ tools/slurm/packer/scripts/generate-pkr-vars.sh
@@ -7,13 +7,14 @@
 # is downloaded from an HPE internal registry.
 
 # Base URL of the hpe-hpc-launcher release tree to download from if necessary
-ARTIFACT_BASE_URL=https://arti.hpc.amslabs.hpecorp.net/artifactory/analytics-misc-stable-local/release/
+ARTIFACT_BASE_URL=https://arti.hpc.amslabs.hpecorp.net/artifactory/analytics-misc-stable-local/ard-capsules-core/
 
 # Checks the build directory for any debian files. If there is no launcher debians,
 # the latest launcher version is downloaded. Otherwise, the debian in build/ is used
 CURRENT_VERSION=$(ls build/ | grep hpe-hpc-launcher | grep .deb)
-# Runs a curl command that sorts all of the versions on artifactory and chooses the latest one
-LATEST_VERSION=$(curl -X GET $ARTIFACT_BASE_URL | sed 's/<[^>]*>//g' | grep "^[1-9]" | tail -n 1 | cut -d/ -f1)
+# Runs a curl command that gets all the debian files from artifactory, filters release versions (n.n.n-n),
+# sorts the versions in reverse order and chooses the latest one version.
+LATEST_VERSION=$(curl -sX GET $ARTIFACT_BASE_URL | sed 's/<[^>]*>//g' | grep hpe-hpc-launcher | grep -E '[0-9]+(\.[0-9]+){2}\-[0-9]\_' | sort -r --version-sort | head -n 1 | cut -d' ' -f1)
 
 if [ -n "$CURRENT_VERSION" ]; then
     # If current version exists and it's not the latest version, prompt the user for action
@@ -21,9 +22,9 @@ if [ -n "$CURRENT_VERSION" ]; then
         read -p "Your launcher version is out of date. Do you want to overwrite the outdated version with the new version? (y/n): " answer
         case ${answer:0:1} in
             y | Y)
-                echo >&2 "INFO: Downloading hpe-hpc-launcher_${LATEST_VERSION}.deb"
+                echo >&2 "INFO: Downloading $LATEST_VERSION"
                 rm -rf build/$CURRENT_VERSION
-                wget -P build/ $ARTIFACT_BASE_URL$LATEST_VERSION/rocky_9_0/${LATEST_VERSION: -1}-0_amd64/hpe-hpc-launcher_$LATEST_VERSION-0_amd64.deb
+                wget -P build/ $ARTIFACT_BASE_URL/$LATEST_VERSION
                 CURRENT_VERSION=$(ls build/ | grep hpe-hpc-launcher | grep .deb)
                 ;;
             *)
@@ -34,8 +35,8 @@ if [ -n "$CURRENT_VERSION" ]; then
         echo >&2 "INFO: Using existing ${CURRENT_VERSION}"
     fi
 elif [ -z "$CURRENT_VERSION" ]; then
-    echo >&2 "INFO: Downloading hpe-hpc-launcher_${LATEST_VERSION}.deb"
-    wget -P build/ $ARTIFACT_BASE_URL$LATEST_VERSION/rocky_9_0/${LATEST_VERSION: -1}-0_amd64/hpe-hpc-launcher_$LATEST_VERSION-0_amd64.deb
+    echo >&2 "INFO: Downloading $LATEST_VERSION"
+    wget -P build/ $ARTIFACT_BASE_URL/$LATEST_VERSION
     CURRENT_VERSION=$(ls build/ | grep hpe-hpc-launcher | grep .deb)
 fi
 
