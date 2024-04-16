tayritenour/determined

feat: slurm support (#98)
This change adds a dispatcher resource manager, which gives Determined the ability to run on top of Slurm.

Co-authored-by: rcorujo <90728398+rcorujo@users.noreply.github.com>
Co-authored-by: Phillip Gaisford <phillip.gaisford@hpe.com>
Co-authored-by: phillip-gaisford <98362331+phillip-gaisford@users.noreply.github.com>
Co-authored-by: Jerry J. Harrow <84593277+jerryharrow@users.noreply.github.com>
Co-authored-by: Jagadeesh Madagundi <jagadeesh545@gmail.com>
Co-authored-by: Philip Norman <philipnrmn@users.noreply.github.com>

master/internal/config/resource_config.go
@@ -22,7 +22,9 @@ func (r *ResourceConfig) ResolveResource() error {
 			AgentRM: &AgentResourceManagerConfig{},
 		}
 	}
-	if r.ResourceManager.AgentRM == nil && r.ResourceManager.KubernetesRM == nil {
+	if r.ResourceManager.AgentRM == nil &&
+		r.ResourceManager.KubernetesRM == nil &&
+		r.ResourceManager.DispatcherRM == nil {
 		r.ResourceManager.AgentRM = &AgentResourceManagerConfig{}
 	}
 	if r.ResourcePools == nil &&

determined-ai/determined-release-testing

master/internal/config/resource_config.go
@@ -75,11 +75,15 @@ func (r *ResourceConfig) ResolveResource() error {
 	// Add a default resource manager.
 	// I'm not sure if this code could ever be true, since we default in UnmarshalJSON.
 	// This feels risky to remove though.
-	if r.RootManagerInternal.AgentRM == nil && r.RootManagerInternal.KubernetesRM == nil {
+	if r.RootManagerInternal.AgentRM == nil &&
+		r.RootManagerInternal.KubernetesRM == nil &&
+		r.RootManagerInternal.DispatcherRM == nil {
 		r.RootManagerInternal.AgentRM = defaultAgentRM()
 	}
 	for _, c := range r.AdditionalResourceManagersInternal {
-		if c.ResourceManager.AgentRM == nil && c.ResourceManager.KubernetesRM == nil {
+		if c.ResourceManager.AgentRM == nil &&
+			c.ResourceManager.KubernetesRM == nil &&
+			c.ResourceManager.DispatcherRM == nil {
 			// This error should be impossible to go off.
 			return fmt.Errorf("please specify an resource manager type")
 		}