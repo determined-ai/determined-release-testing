tayritenour/determined

chore: config pbs resource manager type. (#416)

master/internal/config/resource_config.go
@@ -24,7 +24,8 @@ func (r *ResourceConfig) ResolveResource() error {
 	}
 	if r.ResourceManager.AgentRM == nil &&
 		r.ResourceManager.KubernetesRM == nil &&
-		r.ResourceManager.DispatcherRM == nil {
+		r.ResourceManager.DispatcherRM == nil &&
+		r.ResourceManager.PbsRM == nil {
 		r.ResourceManager.AgentRM = &AgentResourceManagerConfig{}
 	}
 	if r.ResourcePools == nil &&

determined-ai/determined-release-testing

master/internal/config/resource_config.go
@@ -77,7 +77,8 @@ func (r *ResourceConfig) ResolveResource() error {
 	// This feels risky to remove though.
 	if r.RootManagerInternal.AgentRM == nil &&
 		r.RootManagerInternal.KubernetesRM == nil &&
-		r.RootManagerInternal.DispatcherRM == nil {
+		r.RootManagerInternal.DispatcherRM == nil &&
+		r.RootManagerInternal.PbsRM {
 		r.RootManagerInternal.AgentRM = defaultAgentRM()
 	}
 	for _, c := range r.AdditionalResourceManagersInternal {